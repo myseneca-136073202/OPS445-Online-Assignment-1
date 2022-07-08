dfd38f21debc4825b88206e7e1d1e1f591dee0be
czhu30 <105614897+czhu30@users.noreply.github.com>
2022-07-07 23:10:57 -0400

    Update assign1.py

--- assign1.py
+++ assign1.py
@@ -5,10 +5,10 @@ import sys
 OPS445 Assignment 1 - Winter 2022
 
 Program: assign1.py
-Author: "Student Full Name" - "Student ID"
+Author: Chuanlong Zhu - 105009211
 
 The python code in this file (assign1.py) is original work written by
-"Student Name". No code in this file is copied from any other source 
+Chuanlong Zhu. No code in this file is copied from any other source 
 except those provided by the course instructor, including any person, 
 textbook, or on-line resource. I have not shared this python script 
 with anyone or anything except for submission for grading.  
@@ -17,7 +17,7 @@ violators will be reported and appropriate action will be taken.
 
 Description: <Enter your documentation here>
 
-Date: 2022-02-06
+Date: 2022-07-06
 '''
 
 from asyncio.windows_events import NULL
@@ -30,8 +30,22 @@ def usage():
 
 def days_in_mon(year):
     "TODO enter docstring"
-    # return dictionary_months
-    pass # TODO: delete this line, replace with valid code.
+    lyear = year % 4 # TODO: put this into the function leap_year.
+    if lyear == 0:
+        feb_max = 29 # this is a leap year
+    else:
+        feb_max = 28 # this is not a leap year
+
+    lyear = year % 100
+    if lyear == 0:
+        feb_max = 28 # this is not a leap year
+
+    lyear = year % 400
+    if lyear == 0:
+        feb_max = 29 # this is a leap year
+
+    mon_max = { 1:31, 2:feb_max, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    return mon_max
 
 def valid_date(date):
     "TODO enter docstring"
@@ -107,7 +121,7 @@ def after(today):
 
         tmp_day = day + 1 # next day
 
-        mon_max = { 1:31, 2:feb_max, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+        mon_max = days_in_mon(year)
         if tmp_day > mon_max[month]:
             to_day = tmp_day % mon_max[month] # if tmp_day > this month's max, reset to 1
             tmp_month = month + 1
